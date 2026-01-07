5f9141a5cdcdaf5ac4f8621560ee53b9da846387
anuragpujari <anurag.pujari123@gmail.com>
2026-01-07 10:58:53 +0530

    updated base URL

---
+++ assets/js/abeffa68.f8248002.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkmy_website=globalThis.webpackChunkmy_website||[]).push([[7222],{7360(e){e.exports=JSON.parse('{"metadata":{"permalink":"/documentation.fluidzy/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://anurag-pujari.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/documentation.fluidzy/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
